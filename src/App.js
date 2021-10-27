beacac9cec1f5501a683a5234f306c837c36984f
Your Name <prayushdave@gmail.com>
2021-10-27 23:07:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import FlashcardList from './FlashcardList';
 
-function App() {
+function App(){
+  // eslint-disable-next-line
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlashcardList flashcards={flashcards}></FlashcardList>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'Which movie trailer did you like the most?',
+    answer: 'Infinity',
+    options: [
+      'cowboy bebop',
+      'dune',
+      'venom',
+      'Infinity'
+    ]
+  },
+  {
+    id: 2,
+    question: 'question2?',
+    answer: 'answer',
+    options: [
+      'option1',
+      'option2',
+      'option3',
+      'option4'
+    ]
+  }
+]
 
 export default App;
---
+++ src/Flashcard.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+export default function Flashcard({flashcard}) {
+    const [flip, setFlip] = useState(false)
+    return (
+        <div onClick={() => setFlip(!flip)}>
+            {flip ? flashcard.answer : flashcard.question}
+        </div>
+    )
+}
