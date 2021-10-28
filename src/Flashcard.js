86cff913edd3ea559f91d5add07c73a4339a668a
Your Name <prayushdave@gmail.com>
2021-10-28 23:23:15 +0530

    Flashcards CSS completed

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -3,8 +3,16 @@ import React, {useState} from 'react'
 export default function Flashcard({flashcard}) {
     const [flip, setFlip] = useState(false)
     return (
-        <div onClick={() => setFlip(!flip)}>
-            {flip ? flashcard.answer : flashcard.question}
+        <div className={`card ${flip ? 'flip' : ''}`} onClick={()=>{setFlip(!flip)}}> 
+                <div className='front'>
+                    <div className="flashcard-question">{flashcard.question}</div>
+                        <div className="flashcard-options">
+                            {flashcard.options.map((option) => {
+                                return <div className="flashcard-option">{option}</div>
+                            })}
+                        </div>
+                    </div>
+                <div className="back">{flashcard.answer}</div>
         </div>
     )
 }
