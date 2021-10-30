f23110a7723ef7102e07cf852b759076580256dd
Your Name <prayushdave@gmail.com>
2021-10-30 16:27:00 +0530

    version1 completed

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,18 +1,37 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect } from 'react'
+
 
 export default function Flashcard({flashcard}) {
     const [flip, setFlip] = useState(false)
+    const [height, setHeight] = useState(50)
+    const frontEl = useRef()
+    const backEl = useRef()
+
+    function setMaxHeight(){
+        const frontHeight = frontEl.current.getBoundingClientRect().height
+        const backHeight = backEl.current.getBoundingClientRect().height
+        setHeight(Math.max(frontHeight, backHeight, 100))
+    }
+
+    useEffect(setMaxHeight,
+        [flashcard.question, flashcard.options, flashcard.answer])
+
+    useEffect(()=>{
+        window.addEventListener('resize', setMaxHeight)
+        return window.removeEventListener('resize', setMaxHeight)
+    }, [])
+    
     return (
-        <div className={`card ${flip ? 'flip' : ''}`} onClick={()=>{setFlip(!flip)}}> 
-                <div className='front'>
+        <div className={`card ${flip ? 'flip' : ''}`} style={{height: height}} onClick={()=>{setFlip(!flip)}}> 
+                <div className='front' ref={frontEl}>
                     <div className="flashcard-question">{flashcard.question}</div>
                         <div className="flashcard-options">
                             {flashcard.options.map((option) => {
-                                return <div className="flashcard-option">{option}</div>
+                                return <div className="flashcard-option" key={option}>{option}</div>
                             })}
                         </div>
                     </div>
-                <div className="back">{flashcard.answer}</div>
+                <div className="back" ref={backEl}>{flashcard.answer}</div>
         </div>
     )
 }
